e9f9a67396221f8de0f3972bd88da541a9612b66
A7meKamaL11 <enginer.a.kamal@gmail.com>
2022-10-27 03:56:55 +0200

    delete tasks update and finish App

--- lib/shared/components/constants.dart
+++ lib/shared/components/constants.dart
@@ -1,1 +1,15 @@
-List<Map> tasks=[];
+import 'dart:ui';
+
+List<Map> tasks = [];
+
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+}
